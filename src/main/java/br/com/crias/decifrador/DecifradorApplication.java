55cefe7cdbd8d280b0f6b0d30ee0c75f970e7db9
jhonatapers <jhonatapers@gmail.com>
2024-03-13 13:18:10 -0300

    projeto base

---
+++ src/main/java/br/com/crias/decifrador/DecifradorApplication.java
@@ -0,0 +1,13 @@
+package br.com.crias.decifrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecifradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecifradorApplication.class, args);
+	}
+
+}
