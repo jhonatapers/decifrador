c5cf594c321505454db374f79cdeeb2c371dea94
jhonatapers <jhonatapers@gmail.com>
2024-03-27 20:01:10 -0300

    Fix quebra de subtextos
    Co-authored-by: Henrique Baptista de Oliveira <HBO1705@users.noreply.github.com>

--- src/main/java/br/com/crias/decifrador/Vigenere.java
+++ src/main/java/br/com/crias/decifrador/Vigenere.java
@@ -11,9 +11,9 @@ import lombok.RequiredArgsConstructor;
 @Component
 public class Vigenere implements Decifrador {
 
-    private final Double indiceCoincidenciaAlvo = 1.73;
+    private final Double indiceCoincidenciaAlvo = 0.066;
 
-    private final Double margem = 0.2;
+    private final Double margem = 0.002;
 
     private final CalculadoraDeCoincidencia calculadoraDeCoincidencia;
 
@@ -21,20 +21,21 @@ public class Vigenere implements Decifrador {
     public String decifrar(String textoCifrado) {
 
         int tamanhoChave = 1;
+        String[] subTextos = capturarSubTextosPorTamanhoDaChave(textoCifrado, tamanhoChave);
 
-        while (!dentroDaMargem(calcularIndiceCoincidencia(textoCifrado, tamanhoChave))) {
+        while (!dentroDaMargem(calcularIndiceCoincidencia(subTextos))) {
             if (tamanhoChave == textoCifrado.length())
                 break;
+
             tamanhoChave++;
+            subTextos = capturarSubTextosPorTamanhoDaChave(textoCifrado, tamanhoChave);
         }
 
         return "";
     }
 
-    private double calcularIndiceCoincidencia(String textoCifrado, int tamanhoChave) {
+    private double calcularIndiceCoincidencia(String[] subTextos) {
         Collection<Double> indicesCoincidencias = new ArrayList<>();
-
-        String[] subTextos = capturarSubTextosPorTamanhoDaChave(textoCifrado, tamanhoChave);
         for (int i = 0; i < subTextos.length; i++)
             indicesCoincidencias.add(calculadoraDeCoincidencia.calcular(subTextos[i]));
 
@@ -50,7 +51,7 @@ public class Vigenere implements Decifrador {
         int i = 0;
         for (Character character : texto.toCharArray()) {
             builders[i].append(character);
-            i = i == tamanhoChave ? 0 : i++;
+            i = i == tamanhoChave - 1 ? 0 : i + 1;
         }
 
         String[] subTextos = new String[tamanhoChave];
